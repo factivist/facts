fc52b28efc35b1eed675378755e2f332829d2db8
Umar Nawaz <unawaz.developer@gmail.com>
2015-10-24 09:15:42 -0400

    update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
-./copy.sh
+cp /home/umar/public_html/umar_ideas.html ideas.html
+cp /home/umar/public_html/kaggle_pay.png .
 git add *
 git add pages/*
-git commit -a -m 'sdf'
+git commit -a -m 'update.sh'
 git push origin gh-pages
