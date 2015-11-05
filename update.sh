93add140249c918752e3912a7bf3d7028f090d87
Umar Nawaz <unawaz.developer@gmail.com>
2015-11-05 18:16:35 -0500

    update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 cp /home/umar/public_html/ideas.html .
 cp /home/umar/public_html/*.png .
 cp /home/umar/public_html/*.jpg .
+cp /home/umar/public_html/*.css .
 git add *
 git add pages/*
 git commit -a -m 'update.sh'
