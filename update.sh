2c459472d8b75f4d9364bafaa45360acd03c4f89
Umar Nawaz <unawaz.developer@gmail.com>
2015-11-05 18:15:54 -0500

    update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
-cp /home/umar/public_html/umar_ideas.html ideas.html
-cp /home/umar/public_html/kaggle_pay.png .
+cp /home/umar/public_html/ideas.html .
+cp /home/umar/public_html/*.png .
+cp /home/umar/public_html/*.jpg .
 git add *
 git add pages/*
 git commit -a -m 'update.sh'
